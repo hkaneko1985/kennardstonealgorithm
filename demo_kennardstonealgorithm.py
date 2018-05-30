a6c4fd6a23373c8b3e0e9369f982d7e5dbcaa4e9
Hiromasa Kaneko <31059982+hkaneko1985@users.noreply.github.com>
2018-05-30 19:28:05 +0900

    Add files via upload

--- demo_kennardstonealgorithm.py
+++ demo_kennardstonealgorithm.py
@@ -4,31 +4,34 @@
 Demonstration of Kennard-Stone algorighm
 """
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+
 import kennardstonealgorithm
 
-numberofsamples = 50
-numberofselectedsamples = 20
+number_of_samples = 50
+number_of_selected_samples = 20
 
 # generate samples 0f samples for demonstration
-X = np.random.rand(numberofsamples,2)
+X = np.random.rand(number_of_samples, 2)
 
 # standarize X
 autoscaledX = (X - X.mean()) / X.std(ddof=1)
 
 # select samples using Kennard-Stone algorithm
-selectedsamplenumbers, remainingsamplenumbers = kennardstonealgorithm.kennardstonealgorithm( autoscaledX, numberofselectedsamples )
-print( "selected sample numbers")
-print( selectedsamplenumbers )
-print( "---" )
-print( "remaining sample numbers" )
-print( remainingsamplenumbers )
+selected_sample_numbers, remaining_sample_numbers = kennardstonealgorithm.kennardstonealgorithm(
+    autoscaledX, number_of_selected_samples)
+print("selected sample numbers")
+print(selected_sample_numbers)
+print("---")
+print("remaining sample numbers")
+print(remaining_sample_numbers)
 
 # plot samples
 plt.figure()
-plt.scatter( autoscaledX[:,0], autoscaledX[:,1], label = "all samples")
-plt.scatter( autoscaledX[selectedsamplenumbers,0], autoscaledX[selectedsamplenumbers,1], marker="*", label = "all samples")
+plt.scatter(autoscaledX[:, 0], autoscaledX[:, 1], label="all samples")
+plt.scatter(autoscaledX[selected_sample_numbers, 0], autoscaledX[selected_sample_numbers, 1], marker="*",
+            label="all samples")
 plt.xlabel("x1")
 plt.ylabel("x2")
 plt.legend(loc='upper right')
--- kennardstonealgorithm.py
+++ kennardstonealgorithm.py
@@ -1,35 +1,38 @@
 # select samples using Kennard-Stone algorithm
 import numpy as np
 
+
 # --- input ---
 # X : dataset of X-variables (samples x variables)
 # k : number of samples to be selected
 #
 # --- output ---
-# selectedsamplenumbers : selected sample numbers (training data)
-# remainingsamplenumbers : remaining sample numbers (test data)
+# selected_sample_numbers : selected sample numbers (training data)
+# remaining_sample_numbers : remaining sample numbers (test data)
 
-def kennardstonealgorithm( X, k ):
-    X = np.array( X )
-    originalX = X
-    distancetoaverage = ( (X - np.tile(X.mean(axis=0), (X.shape[0], 1) ) )**2 ).sum(axis=1)
-    maxdistancesamplenumber = np.where( distancetoaverage == np.max(distancetoaverage) )
-    maxdistancesamplenumber = maxdistancesamplenumber[0][0]
-    selectedsamplenumbers = list()
-    selectedsamplenumbers.append(maxdistancesamplenumber)
-    remainingsamplenumbers = np.arange( 0, X.shape[0], 1)
-    X = np.delete( X, selectedsamplenumbers, 0)
-    remainingsamplenumbers = np.delete( remainingsamplenumbers, selectedsamplenumbers, 0)
+def kennardstonealgorithm(x_variables, k):
+    x_variables = np.array(x_variables)
+    original_x = x_variables
+    distance_to_average = ((x_variables - np.tile(x_variables.mean(axis=0), (x_variables.shape[0], 1))) ** 2).sum(axis=1)
+    max_distance_sample_number = np.where(distance_to_average == np.max(distance_to_average))
+    max_distance_sample_number = max_distance_sample_number[0][0]
+    selected_sample_numbers = list()
+    selected_sample_numbers.append(max_distance_sample_number)
+    remaining_sample_numbers = np.arange(0, x_variables.shape[0], 1)
+    x_variables = np.delete(x_variables, selected_sample_numbers, 0)
+    remaining_sample_numbers = np.delete(remaining_sample_numbers, selected_sample_numbers, 0)
     for iteration in range(1, k):
-        selectedsamples = originalX[selectedsamplenumbers,:]
-        mindistancetoselectedsamples = list()
-        for mindistancecalculationnumber in range( 0, X.shape[0]):
-            distancetoselectedsamples = ( (selectedsamples - np.tile(X[mindistancecalculationnumber,:], (selectedsamples.shape[0], 1)) )**2 ).sum(axis=1)
-            mindistancetoselectedsamples.append( np.min(distancetoselectedsamples) )
-        maxdistancesamplenumber = np.where( mindistancetoselectedsamples == np.max(mindistancetoselectedsamples) )
-        maxdistancesamplenumber = maxdistancesamplenumber[0][0]
-        selectedsamplenumbers.append(remainingsamplenumbers[maxdistancesamplenumber])
-        X = np.delete( X, maxdistancesamplenumber, 0)
-        remainingsamplenumbers = np.delete( remainingsamplenumbers, maxdistancesamplenumber, 0)
+        selected_samples = original_x[selected_sample_numbers, :]
+        min_distance_to_selected_samples = list()
+        for min_distance_calculation_number in range(0, x_variables.shape[0]):
+            distance_to_selected_samples = ((selected_samples - np.tile(x_variables[min_distance_calculation_number, :],
+                                                                        (selected_samples.shape[0], 1))) ** 2).sum(axis=1)
+            min_distance_to_selected_samples.append(np.min(distance_to_selected_samples))
+        max_distance_sample_number = np.where(
+            min_distance_to_selected_samples == np.max(min_distance_to_selected_samples))
+        max_distance_sample_number = max_distance_sample_number[0][0]
+        selected_sample_numbers.append(remaining_sample_numbers[max_distance_sample_number])
+        x_variables = np.delete(x_variables, max_distance_sample_number, 0)
+        remaining_sample_numbers = np.delete(remaining_sample_numbers, max_distance_sample_number, 0)
 
-    return selectedsamplenumbers, remainingsamplenumbers
+    return selected_sample_numbers, remaining_sample_numbers
